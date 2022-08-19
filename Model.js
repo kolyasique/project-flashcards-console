b9af9477c86ceb8329cf2300e89a3ad17d447754
aav2581 <aav2581@gmail.com>
2022-08-19 16:34:11 +0300

    clean version

--- Model.js
+++ Model.js
@@ -4,9 +4,8 @@ class Model {
   constructor() {
     this.topicsMenu = ['1. Ночные ястребы', '2. Выдра', '3. Еноты'];
     this.topics = [];
-    this.answers = []; //this.answersFromFile(topic)
-    this.questions = []; //this.questionsFromFile(topic)
-    // this.questionsAndAnswers = this.questionsFromFile(topic)
+    this.answers = []; 
+    this.questions = []; 
     this.score = 0;
     this.choosenNumber = 0;
     this.answersAll = [];
@@ -20,30 +19,15 @@ class Model {
   async fillQuestions(file) {
     const filetext = await (await fs.readFile(`./topics/${file}`, "utf-8")).split("\n");
     let arrayOfQuestions = await filetext.filter((el, id) => id % 3 === 0);
-    this.questions = arrayOfQuestions; // await filetext.filter((el, id) => id % 3 === 0);
-    return arrayOfQuestions; // нужен только для проверки
+    this.questions = arrayOfQuestions;
   }
 
   async fillAnswers(file) {
     const filetext = await (await fs.readFile(`./topics/${file}`, "utf-8")).split("\n");
-    let arr2 =[]
     let arrayOfAnswers = await filetext.filter((el, id) => el != "").filter((el, id) => id % 2 != 0);
     this.answers = arrayOfAnswers;
     return arrayOfAnswers;
   }
-
-  async countscore(arrRigthRes, arrResults) {
-      const counter = arrRigthRes.filter((el, index) => el === arrResults[index]).length;
-      this.score = counter;
-  }
-
-  // async qAndA() {
-  //   this.allQA  =  await this.questions.map((el, i) => {
-  //     return {el: this.answers[i]};
-  //     });
-  // }
-
-
 }
 
 
