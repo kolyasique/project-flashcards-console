b980193beb5ba31a09290800c866bbcf198783cc
nikolayvadimovich <a111@MacBook-Pro-111.local>
2022-08-19 14:42:24 +0300

    Model_processing

--- Controller.js
+++ Controller.js
@@ -1,21 +1,27 @@
 class Controller {
   constructor(model, view) {
-    this.model = model
-    this.view = view
+    this.model = model;
+    this.view = view;
   }
 
-  run() {
+  async run() {
+    await this.model.fillTopics();
+    this.view.showTopics(this.createTopicsMenu())
     // Просим экземпляр класса модели прочитать папку со всеми темами и составить меню.
     // Попутно передаем метод контроллера this.printTopicsController,
     // так как нам нужно отправить сформинованное меню на вывод в экземпляр класса view
     // после того, как завершится асинхронная операция чтения папки
     // Здесь this.printTopicsController — является callback'ом  
-    this.model.readTopics(this.printTopicsController)
+    // this.model.readTopics(this.printTopicsController)
   }
 
   printTopicsController(topicsMenu) {
     // Тут нужно попросить экземпляр класса view вывести меню пользователю, 
     // а также дождаться ответа последнего
+  }
+
+  createTopicsMenu(){
+    return this.model.topics.map((topic, i) => `${i+1}. ${topic}`).join('\n');
   }
 
   
--- View.js
+++ View.js
@@ -2,6 +2,10 @@ class View {
   constructor() {
 
   }
+
+  showTopics(topicsMenu){
+    console.log(topicsMenu);// rl выводит на экран меню и потом дожидается ответа пользователя и далееююю
+  }
 }
 
 module.exports = View
